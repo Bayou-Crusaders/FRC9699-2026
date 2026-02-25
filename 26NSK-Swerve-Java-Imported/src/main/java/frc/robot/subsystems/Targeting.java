5e876b3533772393b846d728cb9be4a22c599704
Brayden Durocher <braydendurocher5009@gmail.com>
2026-02-24 18:21:26 -0600

    Update Targeting.java

--- 26NSK-Swerve-Java-Imported/src/main/java/frc/robot/subsystems/Targeting.java
+++ 26NSK-Swerve-Java-Imported/src/main/java/frc/robot/subsystems/Targeting.java
@@ -3,10 +3,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 import frc.robot.Util;
 
